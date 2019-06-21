17645579269906784b0dd223d9dd87947e78b6ed
himkwtn <him10424@gmail.com>
2019-06-21 18:17:10 +0700

    style(example): rename files

--- src/main/kotlin/org/thinc/demo/example/ExampleServiceTest.kt
+++ src/main/kotlin/org/thinc/demo/example/ExampleServiceTest.kt
@@ -8,7 +8,7 @@ import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
-class ServiceTest {
+class ExampleServiceTest {
 
     @Mock
     lateinit var repository: CatRepository
