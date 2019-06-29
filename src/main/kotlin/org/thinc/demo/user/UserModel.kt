b79ddd1004fc48ad9547dd4c72b7624426132cf7
himkwtn <him10424@gmail.com>
2019-06-29 16:02:31 +0700

    feat(user): update data model

--- src/main/kotlin/org/thinc/demo/user/UserModel.kt
+++ src/main/kotlin/org/thinc/demo/user/UserModel.kt
@@ -4,8 +4,12 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class User(@Id val id: String? = null, val name_th: Name, val name_en: Name, val contact: Contact, val health: Health,
-                val studentInfo: StudentInfo)
+data class User(@Id val id: String? = null, val gender: GENDER, val name_th: Name, val name_en: Name,
+                val contact: Contact, val health: Health, val studentInfo: StudentInfo)
+
+enum class GENDER {
+    MALE, FEMALE
+}
 
 data class Name(val firstName: String, val lastName: String, val nickName: String)
 data class Contact(val phone: String, val emergency: String, val email: String, val address: String,
@@ -16,4 +20,4 @@ enum class BLOODTYPE {
     A, B, AB, O
 }
 
-data class StudentInfo(val studentID: String, val major: String)
+data class StudentInfo(val studentID: String, val major_en: String, val major_th: String)
